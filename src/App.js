70e41d0c8c5226cc0356d750a54a9f513d827b9d
Raihan <rhnchdri@gmail.com>
2023-10-19 10:03:03 +0600

    count add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculation></Calculation>
+    </div>
+  );
+}
+
+function Calculation() {
+
+  const [count, setCount] = useState(0);
+
+  // Number increasing
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  // const increaseCount = () => setCount(count + 1);
+
+
+  // Number decreasing
+  const decreaseCount = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  }
+  // const decreaseCount = () => setCount(count - 1)
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button className='increase' onClick={increaseCount}>Increase</button>
+      <br />
+      <button className='decrease' onClick={decreaseCount}>Decrease</button>
     </div>
   );
 }
